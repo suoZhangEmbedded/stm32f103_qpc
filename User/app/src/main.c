5478512c3f0cc754f171eb5a73bf36b391224237
suoZhangEmbedded@gmail.com <suozhangembedded@gmail.com>
2019-08-23 13:29:49 +0800

    1、git init

---
+++ User/app/src/main.c
@@ -0,0 +1,92 @@
+
+/*
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-6-11     suozhang      the first version
+ *
+ */
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "stm32f10x.h"
+
+#include "SEGGER_RTT.h"
+#include "stdio.h"
+
+/**
+ * 重定义fputc函数
+ *
+ * @param 输出的字符
+ * @param 目标文件
+ *
+ * @return ch
+ */
+int fputc(int ch, FILE *f)
+{
+
+	SEGGER_RTT_Write( 0, &ch, 1 );
+	
+	return ch;
+
+}
+
+void led_task( void *pvParameters );
+
+TaskHandle_t led_task_handle = NULL;
+
+int main(void)
+{
+	
+	/* 优先级分组设置为4 */
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 );
+
+	xTaskCreate( led_task, "led", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+3, &led_task_handle );
+
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+	
+	while(1);
+	
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask, signed char * pcTaskName )
+{
+
+	while(1);
+	
+}
+
+void led_task( void *pvParameters )
+{
+
+	for(;;)
+	{
+		printf( "tick:%u,system heap:%u.\r\n", xTaskGetTickCount(), xPortGetFreeHeapSize() );
+		
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
